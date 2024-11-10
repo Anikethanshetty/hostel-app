89e640a5a07a4ca9bf019c9a7a793c5b1aba4463
BNsrujan <srujanbn123@gmail.com>
2024-11-10 15:28:39 +0530

    added cors

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const app = express()
 const { PrismaClient } = require("@prisma/client")
 require("dotenv").config()
 const PORT = process.env.PORT
-
+const cors = require("cors")
 const prisma = new PrismaClient()
 
 const { studentRouter } = require("./routes/student")
 const { adminRouter } = require("./routes/admin")
 app.use(express.json())
+app.use(cors({
+  origin: "http://localhost:8081",
+  optionsSuccessStatus: 200,
+  credentials: true,
+}))
 
 app.use("/student/v1", studentRouter)
 app.use("/admin/v1", adminRouter)
